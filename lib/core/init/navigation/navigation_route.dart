d9c1523c94ca9e5f5c70b241220638edca5691de
kutsaltekbas <kutsal_tekbass@hotmail.com>
2023-03-28 13:41:06 +0300

    splash screen navigator added

--- lib/core/init/navigation/navigation_route.dart
+++ lib/core/init/navigation/navigation_route.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:mvvm_complete_todo_app/core/constants/navigation/navigation_constants.dart';
 import 'package:mvvm_complete_todo_app/view/homepage/view/homepage_view.dart';
 
+import '../../../view/login/view/login_view.dart';
+import '../../../view/on board/view/onboard_view.dart';
+
 class NavigationRoute {
   static final NavigationRoute _instance = NavigationRoute._init();
 
@@ -13,6 +16,10 @@ class NavigationRoute {
     switch (args.name) {
       case NavigationConstants.HOME_PAGE_VIEW:
       return MaterialPageRoute(builder: (context)=>HomePageView());
+      case NavigationConstants.LOGIN_VIEW:
+      return MaterialPageRoute(builder: (context)=>LoginView());
+      case NavigationConstants.ON_BOARD_VIEW:
+      return MaterialPageRoute(builder: (context)=>OnBoardView());
         
         
        default:
--- lib/view/splash/viewmodel/splash_screen_viewmodel.dart
+++ lib/view/splash/viewmodel/splash_screen_viewmodel.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
+import 'package:mvvm_complete_todo_app/core/constants/navigation/navigation_constants.dart';
+import 'package:mvvm_complete_todo_app/core/init/navigation/navigation_service.dart';
 
 import '../../../core/base/model/base_view_model.dart';
 part 'splash_screen_viewmodel.g.dart';
@@ -9,11 +11,16 @@ abstract class _SplashScreenViewModelBase with Store, BaseViewModel {
 
 @observable
 bool isLoading = false;
+@observable
+NavigationService navigator = NavigationService.instance;
 
   @override
   void setContext(BuildContext context) => viewModelContext = context;
   @override
-  void init() {}
+  Future<void> init() async {
+    await Future.delayed(Duration(seconds: 3));
+    navigator.navigateToPage(path: NavigationConstants.ON_BOARD_VIEW, object: Text(""));
+  }
 
   @action
   void changeIsLoading() {
