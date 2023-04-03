245c4fa4bccc84df8609eb7c2866881779352eb1
Kutsal Tekbas <kutsal_tekbass@hotmail.com>
2023-04-03 11:02:10 +0300

    data model changes

--- lib/model/task_data_model.dart.dart
+++ lib/model/task_data_model.dart.dart
@@ -18,28 +18,37 @@ class TaskDataModel {
 
 class Task {
   String? title;
-  String? body;
   int? taskId;
   bool? isDone;
   String? category;
+  String? description;
+  List<String>? subtask;
 
-  Task({this.title, this.body, this.taskId, this.isDone, this.category});
+  Task(
+      {this.title,
+      this.taskId,
+      this.isDone,
+      this.category,
+      this.description,
+      this.subtask});
 
   Task.fromJson(Map<String, dynamic> json) {
     title = json['title'];
-    body = json['body'];
     taskId = json['taskId'];
     isDone = json['isDone'];
     category = json['category'];
+    description = json['description'];
+    subtask = json['subtask'].cast<String>();
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['title'] = this.title;
-    data['body'] = this.body;
     data['taskId'] = this.taskId;
     data['isDone'] = this.isDone;
     data['category'] = this.category;
+    data['description'] = this.description;
+    data['subtask'] = this.subtask;
     return data;
   }
 }
