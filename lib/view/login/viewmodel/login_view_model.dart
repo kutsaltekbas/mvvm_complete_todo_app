e9f93ebb470d5dd8df6ac4915f9d266968fb370e
Kutsal Tekbas <kutsal_tekbass@hotmail.com>
2023-04-05 18:06:03 +0300

    login service added

--- lib/view/login/viewmodel/login_view_model.dart
+++ lib/view/login/viewmodel/login_view_model.dart
@@ -1,3 +1,7 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 
@@ -7,43 +11,53 @@ import '../../../core/constants/navigation/navigation_constants.dart';
 part 'login_view_model.g.dart';
 
 class LoginViewModel = _LoginViewModelBase with _$LoginViewModel;
+
 abstract class _LoginViewModelBase with Store, BaseViewModel {
+  DatabaseReference db = FirebaseDatabase.instance.ref("user/authenticate");
+  @observable
+  Dio dio = Dio();
 
-@observable
-TextEditingController emailController = TextEditingController();
+  @observable
+  TextEditingController emailController = TextEditingController();
 
+  @observable
+  TextEditingController passwordController = TextEditingController();
 
-@observable
-TextEditingController passwordController = TextEditingController();
+  @observable
+  bool isLoading = false;
 
+  @action
+  void forgotPasswordFunc() {}
 
-@observable
-bool isLoading = false;
+  @action
+  void returnOnboard() {
+    navigator.navigatorKey.currentState!.pop();
+  }
 
-@action
-void forgotPasswordFunc(){
+  @action
+  void login() {
+    if (dataList[0]["eMail"] == emailController.text &&
+        dataList[0]["password"] == "1234") {
+      navigator.navigateToPageClear(
+          path: NavigationConstants.HOME_PAGE_VIEW, object: Text(""));
+    }
+  }
 
-}
-
-@action
-void returnOnboard(){
- navigator.navigatorKey.currentState!.pop();
-}
-
-@action
-void login(){
-  navigator.navigateToPageClear(path: NavigationConstants.HOME_PAGE_VIEW, object: Text(""));
-}
-
-@action 
-void signUp(){
-
-}
+  @action
+  void signUp() {}
 
   @override
   void setContext(BuildContext context) => viewModelContext = context;
+
+  @observable
+  List<Map<String, dynamic>> dataList = [];
   @override
-  void init() {}
+  Future<void> init() async {
+    var response = await dio.get(
+        "https://network-service-test-default-rtdb.europe-west1.firebasedatabase.app/user/authenticate.json");
+    dataList.add(response.data);
+    inspect(response.data);
+  }
 
   @action
   void changeIsLoading() {
